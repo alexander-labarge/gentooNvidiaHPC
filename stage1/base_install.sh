485563dcd4136d390c3206eba523b7966bea9871
Alexander La Barge <alex@labarge.dev>
2023-12-02 08:48:02 -0500

    Comment out explicit declaration of einfo functions

--- stage1/base_install.sh
+++ stage1/base_install.sh
@@ -476,28 +476,29 @@ function install_in_chroot() {
         set -e  # Exit immediately on error
 
         source /etc/profile
+        source /tmp/einfo_timer_util.sh
 
-        function einfo() {
-            local blue='\e[1;34m'   # Light blue
-            local yellow='\e[1;33m' # Yellow
-            local red='\e[1;31m'    # Red
-            local reset='\e[0m'     # Reset text formatting
+        # function einfo() {
+        #     local blue='\e[1;34m'   # Light blue
+        #     local yellow='\e[1;33m' # Yellow
+        #     local red='\e[1;31m'    # Red
+        #     local reset='\e[0m'     # Reset text formatting
 
-            echo -e "${red}----------------------------------------------------------------------------${reset}"
-            echo -e "${blue}[${yellow}$(date '+%Y-%m-%d %H:%M:%S')${blue}] $1${reset}"
-            echo -e "${red}----------------------------------------------------------------------------${reset}"
-        }
+        #     echo -e "${red}----------------------------------------------------------------------------${reset}"
+        #     echo -e "${blue}[${yellow}$(date '+%Y-%m-%d %H:%M:%S')${blue}] $1${reset}"
+        #     echo -e "${red}----------------------------------------------------------------------------${reset}"
+        # }
 
-        function countdown_timer() {
-            for ((i = 3; i >= 0; i--)); do
-                if [ $i -gt 0 ]; then
-                    echo -ne "\r\033[K\e[31mContinuing in \e[34m$i\e[31m seconds\e[0m"
-                else
-                    echo -e "\r\033[K\e[1;34mContinuing\e[0m"
-                fi
-                sleep 1
-            done
-        }
+        # function countdown_timer() {
+        #     for ((i = 3; i >= 0; i--)); do
+        #         if [ $i -gt 0 ]; then
+        #             echo -ne "\r\033[K\e[31mContinuing in \e[34m$i\e[31m seconds\e[0m"
+        #         else
+        #             echo -e "\r\033[K\e[1;34mContinuing\e[0m"
+        #         fi
+        #         sleep 1
+        #     done
+        # }
 
         einfo "Setting up repository configurations..."
         /tmp/setup_repos_conf.sh
@@ -538,7 +539,7 @@ EOF
     # Make the script executable within the chroot environment
     chmod +x "/mnt/gentoo/tmp/chroot_commands.sh"
 
-    # Enter chroot and execute the script
+    # Enter chroot and execute the install script
     chroot /mnt/gentoo /bin/bash -c tmp/chroot_commands.sh || {
         eerror "An error occurred during the chroot execution."
         return 1
