9a946ed1f7796b73e985adda39471a93b9a776eb
Alexander La Barge <alex@labarge.dev>
2023-12-03 10:23:01 -0500

    Remove sudo error from build fstab

--- full_install/utils/build_fstab.sh
+++ full_install/utils/build_fstab.sh
@@ -56,56 +56,56 @@ echo "# <file system> <mount point>   <type>  <options>       <dump>  <pass>" | 
 
 # Root partition
 ROOT_FSTAB_ENTRY="UUID=$(blkid -o value -s UUID /dev/${DRIVE}8) / ext4 defaults 0 1"
-echo "$ROOT_FSTAB_ENTRY" | sudo tee -a /etc/fstab
+echo "$ROOT_FSTAB_ENTRY" | tee -a /etc/fstab
 einfo "Adding / partition to fstab: $ROOT_FSTAB_ENTRY"
 
 countdown_timer
 
 # EFI partition
 EFI_FSTAB_ENTRY="UUID=$(blkid -o value -s UUID /dev/${DRIVE}1) /boot/efi vfat umask=0077 0 1"
-echo "$EFI_FSTAB_ENTRY" | sudo tee -a /etc/fstab
+echo "$EFI_FSTAB_ENTRY" | tee -a /etc/fstab
 einfo "Adding EFI partition to fstab: $EFI_FSTAB_ENTRY"
 
 countdown_timer
 
 # /home partition
 HOME_FSTAB_ENTRY="UUID=$(blkid -o value -s UUID /dev/${DRIVE}2) /home ext4 defaults 0 2"
-echo "$HOME_FSTAB_ENTRY" | sudo tee -a /etc/fstab
+echo "$HOME_FSTAB_ENTRY" | tee -a /etc/fstab
 einfo "Adding /home partition to fstab: $HOME_FSTAB_ENTRY"
 
 countdown_timer
 
 # /var partition
 VAR_FSTAB_ENTRY="UUID=$(blkid -o value -s UUID /dev/${DRIVE}3) /var ext4 defaults 0 2"
-echo "$VAR_FSTAB_ENTRY" | sudo tee -a /etc/fstab
+echo "$VAR_FSTAB_ENTRY" | tee -a /etc/fstab
 einfo "Adding /var partition to fstab: $VAR_FSTAB_ENTRY"
 
 countdown_timer
 
 # /tmp partition
 TMP_FSTAB_ENTRY="UUID=$(blkid -o value -s UUID /dev/${DRIVE}4) /tmp ext4 defaults 0 2"
-echo "$TMP_FSTAB_ENTRY" | sudo tee -a /etc/fstab
+echo "$TMP_FSTAB_ENTRY" | tee -a /etc/fstab
 einfo "Adding /tmp partition to fstab: $TMP_FSTAB_ENTRY"
 
 countdown_timer
 
 # /usr partition
 USR_FSTAB_ENTRY="UUID=$(blkid -o value -s UUID /dev/${DRIVE}5) /usr ext4 defaults 0 2"
-echo "$USR_FSTAB_ENTRY" | sudo tee -a /etc/fstab
+echo "$USR_FSTAB_ENTRY" | tee -a /etc/fstab
 einfo "Adding /usr partition to fstab: $USR_FSTAB_ENTRY"
 
 countdown_timer
 
 # /opt partition
 OPT_FSTAB_ENTRY="UUID=$(blkid -o value -s UUID /dev/${DRIVE}6) /opt ext4 defaults 0 2"
-echo "$OPT_FSTAB_ENTRY" | sudo tee -a /etc/fstab
+echo "$OPT_FSTAB_ENTRY" | tee -a /etc/fstab
 einfo "Adding /opt partition to fstab: $OPT_FSTAB_ENTRY"
 
 countdown_timer
 
 # /var/log partition
 VARLOG_FSTAB_ENTRY="UUID=$(blkid -o value -s UUID /dev/${DRIVE}7) /var/log ext4 defaults 0 2"
-echo "$VARLOG_FSTAB_ENTRY" | sudo tee -a /etc/fstab
+echo "$VARLOG_FSTAB_ENTRY" | tee -a /etc/fstab
 einfo "Adding /var/log partition to fstab: $VARLOG_FSTAB_ENTRY"
 
 countdown_timer
