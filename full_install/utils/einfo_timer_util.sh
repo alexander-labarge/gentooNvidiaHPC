15550073a193e82aed3166e626dddfea26755840
Alexander La Barge <alex@labarge.dev>
2023-12-04 23:03:49 -0500

    fixed error

--- full_install/utils/einfo_timer_util.sh
+++ full_install/utils/einfo_timer_util.sh
@@ -3,16 +3,16 @@
 
 # Define the einfo function for the chroot environment
 function einfo() {
-    local light_green='\e[1;32m'  # Light green
-    local yellow='\e[1;33m'       # Yellow
-    local red='\e[1;31m'          # Red
-    local reset='\e[0m'           # Reset text formatting
+    local blue='\e[1;34m'   # Light blue
+    local yellow='\e[1;33m' # Yellow
+    local red='\e[1;31m'    # Red
+    local reset='\e[0m'     # Reset text formatting
     local hostname=$(hostname)
     local current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
-    local log_file="/opt/install-log-${hostname}-$(date '+%Y-%m-%d').log"
+    local log_file="/install-log-${hostname}-$(date '+%Y-%m-%d').log"
 
     echo -e "${red}------------------------------------------------------------------------------------------------------------${reset}"
-    echo -e "${light_green}[${yellow}${current_datetime}${light_green}] $1${reset}"
+    echo -e "${blue}[${yellow}$(date '+%Y-%m-%d %H:%M:%S')${blue}] $1${reset}"
     echo -e "${red}------------------------------------------------------------------------------------------------------------${reset}"
 
     # Append the log message to the log file
@@ -22,9 +22,9 @@ function einfo() {
 function countdown_timer() {
     for ((i = 3; i >= 0; i--)); do
         if [ $i -gt 0 ]; then
-            echo -ne "\r\033[K\e[31mContinuing in \e[32m$i\e[31m seconds\e[0m"
+            echo -ne "\r\033[K\e[31mContinuing in \e[34m$i\e[31m seconds\e[0m"
         else
-            echo -e "\r\033[K\e[1;32mContinuing\e[0m"
+            echo -e "\r\033[K\e[1;34mContinuing\e[0m"
         fi
         sleep 1
     done
